9d8bfc6c42a8f72182b594ba6b255b556eaf6a67
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-10-19 15:23:14 +1000

    update

---
+++ readyApp/hotels.py
@@ -0,0 +1,85 @@
+from fastapi import Query, Body, APIRouter
+
+from schemas.hotels import Hotel, HotelPATCH, SchemaHotel
+
+router = APIRouter(prefix="/hotels", tags=["Отели"])
+
+hotels = [
+    {"id": 1, "title": "sochi", "name": "sss"},
+    {"id": 2, "title": "dubai", "name": "ddd"},
+    {"id": 3, "title": "moscow", "name": "mmm"},
+]
+
+
+@router.get("/")
+def func():
+    return "Hello, world"
+
+
+@router.get("", description="Здесь описание метода", response_model=list[SchemaHotel])
+def get_hotels(
+    id: int | None = Query(default=None, description="идентификатор отеля"),
+    title: str | None = Query(default=None, description="Название отеля"),
+):
+
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+
+@router.post(
+    "", summary="добавление отеля", description="<h1>Здесь описание метода</h1>"
+)
+def create_hotel(
+    hotel_data: Hotel = Body(
+        openapi_examples={
+            "1": {
+                "summary": "Сочи",
+                "value": {"title": "Отель Сочи Звезда", "name": "Сочи Море"},
+            },
+            "2": {
+                "summary": "Владивосток",
+                "value": {"title": "Отель СПА ВДК", "name": "Ночь Владивостока"},
+            },
+        }
+    )
+):
+    global hotels
+    hotels.append(
+        {"id": hotels[-1]["id"] + 1, "title": hotel_data.title, "name": hotel_data.name}
+    )
+    return {"status": "OK"}
+
+
+@router.put(
+    "/{hotel_id}",
+    summary="замена данных отеля",
+    description="Здесь описание метода",
+)
+def edit_hotel(hotel_id: int, hotel_data: Hotel):
+    global hotels
+    hotels[hotel_id - 1]["title"] = hotel_data.title
+    hotels[hotel_id - 1]["name"] = hotel_data.name
+    return {"status": "OK"}
+
+
+@router.patch("/{hotel_id}", summary="Частичная замена данных отеля")
+def edit_hotel(hotel_id: int | None, hotel_data: HotelPATCH):
+    global hotels
+    if hotel_data.title:
+        hotels[hotel_id - 1]["title"] = hotel_data.title
+    if hotel_data.name:
+        hotels[hotel_id - 1]["name"] = hotel_data.name
+    return {"status": "OK"}
+
+
+@router.delete("/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"}
---
+++ readyApp/schemas/hotels.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+
+
+class Hotel(BaseModel):
+    title: str
+    name: str
+
+
+class HotelPATCH(BaseModel):
+    title: str | None = Field(None)
+    name: str | None = Field(None)
+
+
+class SchemaHotel(BaseModel):
+    id: int
+    title: str
+    name: str
