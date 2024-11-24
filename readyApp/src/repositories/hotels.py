44d7b1de8b6f1a63abbce416c7ba8b0ab94aec86
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-11-24 23:05:51 +1000

    before task

--- readyApp/src/repositories/hotels.py
+++ readyApp/src/repositories/hotels.py
@@ -14,28 +14,6 @@ class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_all(self, location, title, limit, offset):
-        query = select(HotelsOrm)
-        if location:
-            query = query.filter(
-                func.lower(HotelsOrm.location).contains(
-                    location.lower()
-                )  # обработка разных регистров
-            )
-        if title:
-
-            query = query.filter(
-                HotelsOrm.title.ilike(f"%{title}%")
-            )  # ilike ищет вхождения без учета регистра (Сочи = сочи)
-        query = query.limit(limit).offset(offset)
-        print(query.compile(compile_kwargs={"literal_binds": True}))
-
-        result = await self.session.execute(query)
-        return [
-            Hotel.model_validate(hotel, from_attributes=True)
-            for hotel in result.scalars().all()
-        ]
-
     async def get_filtered_by_time(
         self,
         date_from: date,
@@ -50,15 +28,18 @@ class HotelsRepository(BaseRepository):
             .select_from(RoomsOrm)
             .filter(RoomsOrm.hotel_id.in_(rooms_ids_to_get))
         )
+
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotel_ids_to_get))
         if location:
-            hotel_ids_to_get = hotel_ids_to_get.filter(
-                HotelsOrm.location.ilike(f"%{location}%")
-            )
+            query = query.filter(HotelsOrm.location.ilike(f"%{location}%"))
         if title:
-            hotel_ids_to_get = hotel_ids_to_get.filter(
-                HotelsOrm.title.ilike(f"%{title}%")
-            )
-        hotel_ids_to_get = hotel_ids_to_get.limit(pagination.per_page).offset(
+            query = query.filter(HotelsOrm.title.ilike(f"%{title}%"))
+        query = query.limit(pagination.per_page).offset(
             (pagination.page - 1) * pagination.per_page
         )
-        return await self.get_filtered(HotelsOrm.id.in_(hotel_ids_to_get))
+        result = await self.session.execute(query)
+
+        return [
+            Hotel.model_validate(hotel, from_attributes=True)
+            for hotel in result.scalars().all()
+        ]
