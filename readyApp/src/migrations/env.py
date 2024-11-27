549e16d126a47d508b0bb0e6caa6908e210a8340
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-11-27 17:42:30 +1000

    refactor

--- readyApp/src/migrations/env.py
+++ readyApp/src/migrations/env.py
@@ -4,23 +4,20 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
 from src.database import Base
-
-from src.config import settings
-
 from src.models.bookings import BookingsOrm
+from src.models.facilities import FacilitiesOrm
+from src.models.hotels import HotelsOrm
 from src.models.users import UsersOrm
 from src.models.rooms import RoomsOrm
-from src.models.hotels import HotelsOrm
-from src.models.facilities import FacilitiesOrm
+from src.config import settings
+
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option(
-    "sqlalchemy.url", f"{settings.DB_URL}?async_fallback=True"
-)  # settings.DB_URL- адресс к таблице. после ? параметр для синхронной миграции, когда используем асинхронный движок для бд
+config.set_main_option("sqlalchemy.url", f"{settings.DB_URL}?async_fallback=True")#settings.DB_URL- адресс к таблице. после ? параметр для синхронной миграции, когда используем асинхронный движок для бд
 
 
 # Interpret the config file for Python logging.
@@ -78,7 +75,9 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
         with context.begin_transaction():
             context.run_migrations()
