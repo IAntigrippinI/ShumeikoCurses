63d886e08ff5d5c0de504231d8c17ca7f4d109aa
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-11-27 17:42:08 +1000

    add bad rows

--- readyApp/src/models/facilities.py
+++ readyApp/src/models/facilities.py
@@ -5,14 +5,12 @@ from src.database import Base
 
 class FacilitiesOrm(Base):
     __tablename__ = "facilities"
-    __table_args__ = {'extend_existing': True} 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
 
 
 class RoomsFacilitiesOrm(Base):
     __tablename__ = "rooms_facilities"
-    __table_args__ = {'extend_existing': True} 
 
     id: Mapped[int] = mapped_column(primary_key=True)
     room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
