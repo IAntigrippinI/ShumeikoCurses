52cc31606fa0891a620c432e67e0cfc9c64a0fbc
IAntigrippinI <kovalskii.bi@dvfu.ru>
2025-02-16 17:47:10 +1000

    task

--- readyApp/tests/conftest.py
+++ readyApp/tests/conftest.py
@@ -1,15 +1,60 @@
+from httpx import AsyncClient, ASGITransport
 from dotenv import load_dotenv
+from pydantic import TypeAdapter
+
+from src.main import app
+from src.schemas.hotels import HotelAdd
+from src.schemas.rooms import RoomsAdd
+from src.utils.db_manager import DBManager
+
 load_dotenv(".env-test")
 import pytest
+import json
 from src.config import settings
-from src.database import Base, engine_null_pool
+from src.database import Base, engine_null_pool, async_session_maker_null_pool
 from src.models import *
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main():
+def check_test_mode():
+    assert settings.MODE == "TEST"
+
+@pytest.fixture(scope="session", autouse=True)
+async def setup_database(check_test_mode): # эта функция выполнится после выполнения check_test_mode
     print("Я ФИКСТУРА")
     print(engine_null_pool.url)
-    assert settings.MODE == "TEST"
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope='session', autouse=True)
+async def fill_database(setup_database):
+    with open("tests/mock_hotels.json") as f:
+        json_data = json.load(f)
+        hotels = TypeAdapter(list[HotelAdd]).validate_python(json_data)
+    with open("tests/mock_rooms.json") as f:
+        json_data = json.load(f)
+        rooms = TypeAdapter(list[RoomsAdd]).validate_python(json_data)
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        for hotel in hotels:
+            await db.hotels.add(data=hotel)
+        await db.commit()
+        for room in rooms:
+            await db.rooms.add(data=room)
+        await db.commit()
+
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def test_register_user(setup_database):
+    print("Register user")
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test1234") as ac:
+        responce = await ac.post(
+            "/auth/register",
+            json={
+                "email": "kot@maul.ru",
+                "password": "1234"
+            }
+        )
+
+        print(responce.content)
